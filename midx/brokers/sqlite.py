546ebddd1d6cda9e78b8e2b281f40a1ba595e60d
Mike Boers <github@mikeboers.com>
2014-04-10 16:17:12 -0700

    Notes, and a files table

--- midx/brokers/sqlite.py
+++ midx/brokers/sqlite.py
@@ -23,13 +23,25 @@ class SQLiteBroker(object):
 
     def upgrade_schema(self):
         with self._cur() as cur:
-            cur.execute('''CREATE TABLE IF NOT EXISTS main (
+            cur.execute('''CREATE TABLE IF NOT EXISTS sequences (
                 id INTEGER PRIMARY KEY,
                 prefix TEXT NOT NULL,
                 postfix TEXT NOT NULL,
                 start INTEGER NOT NULL,
                 end INTEGER NOT NULL,
                 padding INTEGER
+            )''')
+            cur.execute('''CREATE TABLE IF NOT EXISTS files (
+                id INTEGER PRIMARY KEY,
+                sequence_id INTEGER REFERENCES sequences(id) NOT NULL,
+                file_no INTEGER NOT NULL,
+                inode INTEGER NOT NULL,
+                devive INTEGER NOT NULL,
+                size INTEGER NOT NULL,
+                mtime INTEGER NOT NULL,
+                ctime INTEGER NOT NULL,
+                itime INTEGER NOT NULL, -- last index time
+                checksum TEXT
             )''')
 
     def add(self, sequences, replace=False):
@@ -38,7 +50,7 @@ class SQLiteBroker(object):
         for (prefix, postfix), group in itertools.groupby(sequences, key=key):
             with self._cur() as cur:
 
-                cur.execute('SELECT id, start, end, padding FROM main WHERE prefix = ? AND postfix = ?', [prefix, postfix])
+                cur.execute('SELECT id, start, end, padding FROM sequences WHERE prefix = ? AND postfix = ?', [prefix, postfix])
                 existing = [Sequence(prefix, postfix, start, end, padding, id_) for id_, start, end, padding in cur]
             
                 if replace:
@@ -48,11 +60,11 @@ class SQLiteBroker(object):
 
                 for seq in to_add:
                     if seq.id is None:
-                        cur.execute('''INSERT INTO main
+                        cur.execute('''INSERT INTO sequences
                             (prefix, postfix, start, end, padding)
                             VALUES (?, ?, ?, ?, ?)
                         ''', [prefix, postfix, seq.start, seq.end, seq.padding])
 
                 to_delete = set(seq.id for seq in existing).difference(seq.id for seq in to_add)
                 for id_ in to_delete:
-                    cur.execute('''DELETE FROM main WHERE id = ?''', [id_])
+                    cur.execute('''DELETE FROM sequences WHERE id = ?''', [id_])
